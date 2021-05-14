2e456e9a3b0725430d11caf95d927eed8e88bab5
421408 <christoph.darms@tu-dortmund.de>
2021-05-14 15:02:03 +0200

    added SQL schema and data

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,39 @@
+create table if not exists Taco_Order(
+    id identity,
+    name varchar(50) not null,
+    street varchar(50) not null,
+    city varchar(50) not null,
+    state varchar(50) not null,
+    zip varchar(10) not null,
+    cc_number varchar(16) not null,
+    cc_date varchar(5) not null,
+    cc-cvv varchar(3) not null,
+    placed_at timestamp no null
+);
+
+create table if not exists Taco(
+    id identity,
+    name varchar(50) not null,
+    taco_order bitint not null,
+    taco_order_key bigint not null,
+    created_at timestamp not null
+);
+
+
+create table if not exists Ingredient_Ref(
+    ingredient varchar(4) not null,
+    taco bigint not null,
+    taco_key bigint not null
+);
+
+create table if not exists Ingredient (
+    id varchar(4) not null,
+    name varchar(25) not null,
+    type varchar(10) not null
+);
+
+alter table Taco
+    add foreign key (taco_order) references Taco_Order(id);
+
+alter table Ingredient_Ref
+    add foreign key (ingredient) references Ingredient(id);

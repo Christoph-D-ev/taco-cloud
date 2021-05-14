27c89fa0724d1251144d814038a817923ba7a3e0
421408 <christoph.darms@tu-dortmund.de>
2021-05-14 17:20:40 +0200

    fixed mismatched type in taco

--- src/main/java/tacos/Taco.java
+++ src/main/java/tacos/Taco.java
@@ -1,6 +1,7 @@
 package tacos;
 
 import lombok.Data;
+import tacos.data.IngredientRef;
 
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
@@ -15,10 +16,14 @@ public class Taco {
 
     @NotNull
     @Size(min=1,message = "Add one Ingredient")
-    private List<Ingredient> ingredients;
+    private List<IngredientRef> ingredients;
 
     private Date createdAt = new Date();
 
     private Long id;
+
+    public void addIngredient(Ingredient ingredient){
+        this.ingredients.add(new IngredientRef(ingredient.getId()));
+    }
 
 }
